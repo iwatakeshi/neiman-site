f2696e5855afa78da55d6ae93dc28fab8422dedb
Takeshi <iwatakeshi@gmail.com>
2017-04-21 22:31:34 -0600

    Update: Add models

---
+++ Models/Product.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace neiman_site.Models
+{
+    public class Product
+    {
+        // A product must have an ID
+        public int ID { get; set; }
+        // A product must have a Name
+        public string Name { get; set; }
+        // A product must have a Price
+        public decimal Price { get; set; }
+        // A product must have a Description
+        public string Description { get; set; }
+        // A product must have a Quantity
+        public int Quantity { get; set; }
+
+    }
+}
